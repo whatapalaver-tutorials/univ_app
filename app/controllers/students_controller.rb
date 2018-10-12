d13c51f152432a2fcf8df2ce867417c40a50c6bb
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-12 20:38:15 +0100

    Adding signup form with faded out flash messages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,20 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
 end

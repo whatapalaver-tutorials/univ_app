4238cc857f637dd74e05cdac638b407c1b88076a
Angela Wolff <warriorwomenblog@gmail.com>
2018-10-14 11:54:22 +0100

    Adding has_secure_password functionality and form input for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -40,7 +40,7 @@ class StudentsController < ApplicationController
   private
 
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :password, :passowrd_confirmation)
   end
 
   def set_student
